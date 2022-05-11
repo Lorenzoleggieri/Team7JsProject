28ca0e49b567a1d87d4b936e5406942094ea26b1
Andrea Rizzo <r.andrea2093@gmail.com>
2022-05-11 15:11:11 +0200

    js implemented for cookie, talkanexperts, offcanvas

---
+++ assets/homepage/cookies.mjs
@@ -0,0 +1,25 @@
+  export const cookieForm = () =>{
+    let container = document.createElement('div')
+    container.classList.add('biscotto', 'rounded', 'd-flex', 'flex-column', 'align-items-center')
+    let text = document.createElement('p')
+    text.classList.add('ms-2')
+    text.innerHTML = 'Accetta i nostri cookies per  aiutarci a migliorare il nostro servizio'
+    let btnContainer = document.createElement('div')
+    btnContainer.classList.add('mb-2', 'd-flex', 'gap-2')
+    btnContainer.innerHTML = '<button id="acceptcookie">Accetta</button><button id="rejectcookie">Rifiuta</button>'
+    container.append(text)
+    container.append(btnContainer)
+    document.body.prepend(container)
+
+
+}
+
+export const cookieGen = () =>{
+  let date = new Date(Date.now() + 86400e3)
+  date = date.toUTCString();
+  document.cookie = 'Andrea=persona; SameSite=Lax;  Secure;expires='+date;
+}
+
+
+
+
